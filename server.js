17ca81739bfa6463b6c4c507702dbc0ca5bfb42b
LEAKONO <leakonoemmanuel3@gmail.com>
2025-02-22 14:30:21 +0300

    routing

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
-const express=require("express")
-const app=express()
+const express = require("express");
+const app = express();
 
+app.use(express.json());
 
-app.use(express.json())
-app.get("/",(req,res)=>{
-    res.send("Hello World!")
-})
-const PORT=5000
-app.listen(PORT,()=>{
-    console.log(`Server is running at port ${PORT}`)
-})
+app.get("/", (req, res) => {
+    res.send("Welcome to our Express App!");
+});
+
+app.get("/about", (req, res) => {
+    res.send("This is the About page.");
+});
+
+app.get("/contact", (req, res) => {
+    res.send("Contact us at: express@example.com");
+});
+
+app.get("/users/:name", (req, res) => {
+    const { name } = req.params;
+    res.send(`Hello, ${name}! Welcome to our site.`);
+});
+
+const PORT = 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
